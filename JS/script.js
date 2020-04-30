2ff9fa6557ab85e972cdd0afb828277ab09b22b2
paulmrest <paulmrest@gmail.com>
2020-04-29 17:55:42 -0700

    Added 2 additional questions, tweaked HTML and CSS

--- JS/script.js
+++ JS/script.js
@@ -1,78 +1,199 @@
 'use strict';
 
-alert('Welcome to my page. We\'re going to play a game');
+// Lab 02 - About Me Guessing Game
+alert('Hi there. We\'re going to play a guessing game about me!');
 
-var userName = prompt('What\'s your name?');
+var totalCorrectCounter = 0;
+var userName = prompt('First, let\'s introduce ourselves. What\'s your name?');
 //START-CONSOLE-TESTING
-// console.log('userName: ' + userName);
+console.log('userName: ' + userName);
 //END-CONSOLE-TESTING
+alert('Welcome ' + userName + ', looking good!');
 
-if (userName.toLowerCase().startsWith('a') ||
-  userName.toLowerCase().startsWith('e') ||
-  userName.toLowerCase().startsWith('i') ||
-  userName.toLowerCase().startsWith('o') ||
-  userName.toLowerCase().startsWith('u'))
+var theRealPaul = prompt(userName + ', are you talking to the real Paul right now?');
+//START-CONSOLE-TESTING
+console.log('theRealPaul: ' + theRealPaul);
+//END-CONSOLE-TESTING
+if (theRealPaul.toLowerCase() === 'yes' ||
+    theRealPaul.toLowerCase() === 'y' ||
+    theRealPaul.toLowerCase() === 'yeah')
 {
-  alert('Lucky you didn\'t have to live through The Great Vowel Shift. That would have been most confusing!');
-}
-else if (userName.length === 7)
-{
-  alert('Seven letters in your name. What a charmed life. ' + userName + ', may your stay here continue that.');
-}
-
-if (userName.toLowerCase().startsWith('p'))
-{
-  alert('Welcome to a fellow P-person. Enjoy your stay ' + userName);
+  alert('Of course you are. We\'ve locked him in a room somewhere.');
 }
 else
 {
-  alert('Welcome ' + userName + ', enjoy your stay at my site.');
+  totalCorrectCounter++;
+  alert('Obviously. This is just an algorithim (or is it?).');
 }
 
-var macBethMuppet = prompt('If you were stage a production of The Scottish Play with Muppets, who would play the lead role?');
+var hairEnvy = prompt('Are you envious of Paul\'s hair?');
 //START-CONSOLE-TESTING
-// console.log('macBethMuppet: ' + macBethMuppet);
+console.log('hairEnvy: ' + hairEnvy);
 //END-CONSOLE-TESTING
-
-
-if (macBethMuppet.toLowerCase().includes('kermit'))
+if (hairEnvy.toLowerCase() === 'yes' ||
+    hairEnvy.toLowerCase() === 'y' ||
+    hairEnvy.toLowerCase() === 'yeah')
 {
-  alert('Well, you\'ve got the green with envy part down already.');
-}
-else if (macBethMuppet.toLowerCase().includes('piggy'))
-{
-  alert('Delusions of grandure no pig has reached before!');
-}
-else if (macBethMuppet.toLowerCase().includes('gonzo'))
-{
-  alert('That nose would be perfect for scrubbing sturbborn spots from stained clothing!');
-}
-else {
-  alert('I don\'t know that character, but I\'m just a simple algorithm. I\'m sure ' + macBethMuppet + ' would give a powerful preformance.');
-}
-
-var twoPastaServings = prompt('When making pasta for two, how much is the correct amount of pasta to cook?');
-//START-CONSOLE-TESTING
-// console.log('twoPastaServings: ' + twoPastaServings);
-//END-CONSOLE-TESTING
-var randomNumber = Math.round(Math.random());
-if (randomNumber === 0)
-{
-  alert('Wrong! ' + twoPastaServings + ' is not nearly enough pasta for two people.');
+  totalCorrectCounter++;
+  alert('Valid choice, ' + userName + '. His hair once almost started a bar fight.');
 }
 else
 {
-  alert('Wrong! ' + twoPastaServings + ' is way too much pasta for two people.');
+  alert('Hair security is nice to have, isn\'t it?');
 }
 
-var pastaSauce = prompt('What\'s the best sauce for your ' + twoPastaServings + ' of pasta that you\'ll eat with ' + macBethMuppet + ' while discussing why they should get the part?');
+var arnoldSchwarzenegger = prompt('Did Arnold Schwarzenegger kill Paul\'s grandmother?');
 //START-CONSOLE-TESTING
-// console.log('pastaSauce: ' + pastaSauce);
+console.log('arnoldSchwarzenegger: ' + arnoldSchwarzenegger);
+//END-CONSOLE-TESTING
+if (arnoldSchwarzenegger.toLowerCase() === 'yes' ||
+    arnoldSchwarzenegger.toLowerCase() === 'y' ||
+    arnoldSchwarzenegger.toLowerCase() === 'yeah')
+{
+  totalCorrectCounter++;
+  alert('He did indeed. By being elected as the Governor of California in 2003.');
+}
+else
+{
+  alert('Effects are like ripples sometimes, ' + userName + ', they spread much further than you\'d expect.');
+}
+
+var messier101Spiral = prompt('Can your naked eye see spiral structure in Messier 101 with a 12 inch telescope?');
+//START-CONSOLE-TESTING
+console.log('messier101Spiral: ' + messier101Spiral);
+//END-CONSOLE-TESTING
+if (messier101Spiral.toLowerCase() === 'yes' ||
+    messier101Spiral.toLowerCase() === 'y' ||
+    messier101Spiral.toLowerCase() === 'yeah')
+{
+  totalCorrectCounter++;
+  alert('You know it ' + userName + '!');
+}
+else
+{
+  alert('I don\'t feel like you\'ve really tried at a proper dark sky location. Maybe give that a whirl.');
+}
+
+//Lab03 - Expanding 'About Me' Guessing Games
+
+//Number guessing game
+alert('Next, ' + userName + ', we\'re going to try a number guessing game.');
+//choose a random number >= 1 and <= 20
+const randomNumber = Math.floor(Math.random() * (20 + 1)) + 1;
+const numAllowedGuesses = 4;
+var numGuessCount = 0;
+var numCorrectlyGuessed = false;
+//START-CONSOLE-TESTING
+console.log('randomNumber: ' + randomNumber);
+//END-CONSOLE-TESTING
+while (!numCorrectlyGuessed)
+{
+  var userGuess = prompt('Guess a number between 1 and 20. You have ' +
+  (numAllowedGuesses - numGuessCount) + ' guesses remaining. Enter \'x\' to exit.');
+  //START-CONSOLE-TESTING
+  console.log('userGuess: ' + userGuess);
+  console.log('numGuessCount: ' + numGuessCount);
+  //END-CONSOLE-TESTING
+  if (userGuess.toLowerCase() === 'x')
+  {
+    alert('Thanks for playing! Onto the next game.');
+    break;
+  }
+
+  if (isNaN(parseFloat(userGuess)) || !isFinite(userGuess))
+  {
+    alert('Not a guess. Please enter a numerical value.');
+  }
+  else if (parseInt(userGuess) === randomNumber)
+  {
+    alert('Right! You\'ve guessed it!');
+    numCorrectlyGuessed = true;
+    totalCorrectCounter++;
+  }
+  else if (numGuessCount >= numAllowedGuesses)
+  {
+    alert('You\'ve used up all your guesses. The answer was ' + randomNumber + '. Better luck next time!');
+    break;
+  }
+  else if (parseInt(userGuess) < randomNumber)
+  {
+    numGuessCount++;
+    alert('You\'re too low. Guess again.');
+  }
+  else if (parseInt(userGuess) > randomNumber)
+  {
+    numGuessCount++;
+    alert('You\'re too high. Guess again.');
+  }
+}
+
+//Multiple correct answers guessing game
+alert('Now, ' + userName + ' we\'re going to try a different sort of guessing game!');
+const seanConneryBondFilms = [
+  'Dr. No',
+  'From Russia with Love',
+  'Goldfinger',
+  'Thunderball',
+  'You Only Live Twice',
+  'Bond in Diamonds Are Forever',
+  'Never Say Never Again'
+];
+
+//START-CONSOLE-TESTING
+console.log('seanConneryBondFilms: ' + seanConneryBondFilms);
 //END-CONSOLE-TESTING
 
-var trinket = prompt('Oh no! ' + macBethMuppet + ' just spilled ' + pastaSauce + ' all over your white tablecloth. Since you\'re petty, what meaningless trinket will you pay them with for being your play?');
-//START-CONSOLE-TESTING
-// console.log('trinket: ' + trinket );
-//END-CONSOLE-TESTING
+const bondAllowedGuesses = 6;
+var bondGuessCount = 0;
+var bondCorrectlyGuessed = false;
+while (!bondCorrectlyGuessed)
+{
+  var bondUserGuess = prompt('Name a Sean Connery Bond film. You have ' + (bondAllowedGuesses - bondGuessCount) + ' guesses remaining. Enter \'x\' to exit.');
 
-alert('Whoops! It\'s a few months later and ' + macBethMuppet + ' found out that the ' + trinket + ' you gave them is worth more than you\'ll earn in your entire lifetime. So they\'re not going do your play anymore. Enjoy your ' + twoPastaServings + ' of pasta and ' + pastaSauce + '. Better luck next time ' + userName + '.');
+  //START-CONSOLE-TESTING
+  console.log('bondUserGuess: ' + bondUserGuess);
+  console.log('bondGuessCount: ' + bondGuessCount);
+  //END-CONSOLE-TESTING
+
+  if (bondUserGuess.toLowerCase() === 'x')
+  {
+    alert('Thanks for playing! Bye.');
+    break;
+  }
+
+  //realistically this should be a for-each loop
+  //alternatively, with properly normalized data, we could use array.include()
+  for (var i = 0; i < seanConneryBondFilms.length; i++)
+  {
+    var oneBondFilm = seanConneryBondFilms[i];
+    if (bondUserGuess.toLowerCase() === oneBondFilm.toLowerCase())
+    {
+      bondCorrectlyGuessed = true;
+    }
+  }
+  if (bondCorrectlyGuessed)
+  {
+    totalCorrectCounter++;
+    alert('Correct! ' + bondUserGuess + ' is a Sean Connery Bond film.');
+  }
+  else
+  {
+    bondGuessCount++;
+    alert('Unfortunately ' + bondUserGuess + ' is not a Sean Connery Bond film. Although it sounds like a good one!');
+  }
+
+  if (bondGuessCount >= bondAllowedGuesses)
+  {
+    alert('You\'ve used up all your guesses. Sorry.');
+    break;
+  }
+}
+var prettyPrintSeanConneryBondFilms = '';
+for (var j = 0; j < seanConneryBondFilms.length; j++)
+{
+  prettyPrintSeanConneryBondFilms += '  ' + seanConneryBondFilms[j] + '\n';
+}
+
+alert('For future reference, the complete list of Sean Connery Bond films is: \n' + prettyPrintSeanConneryBondFilms);
+
+alert('Thanks for playing my games. You got a total of ' + totalCorrectCounter + ' questions right.');
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # CF-201-Lab02
-Code Fellows - 201 - Lab 02: About Me, With a Guessing Game
+## Code Fellows - 201 - Lab 02: About Me, With a Guessing Game
+
+An About Me website (minimal information thus far...) with a simple JS guessing game asking five questions about me.
+
+
+# CF-201-Lab03
+## Code Fellows - 201 - Lab 03: Extended "About Me" Guessing Game
+
+Extended guessing game to include the following:
+* A random number guessing game
+* A guessing game with multiple correct answers
+* A notification to the user of how many answers they got "right"
